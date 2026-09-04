96c77b3942fcc5e8dd06c22855f9ccaae2fa5938
Fayaz-FLM <fayaz.sd@frontlinesedutech.com>
2026-09-04 12:13:49 +0530

    feat: Initial Setup Of Staff Management

---
+++ src/test/java/com/flm/staff/StaffManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flm.staff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
